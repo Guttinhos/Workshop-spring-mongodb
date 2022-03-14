5e1a1b5732b55bed808a6b877f17cb114cc24d4e
Gutemberg Mauricio <gutem.mauricio@gmail.com>
2022-03-14 15:50:04 -0300

    Referenciando os posts do usuario

--- src/main/java/com/mauricio/workshopmongomauricio/domain/User.java
+++ src/main/java/com/mauricio/workshopmongomauricio/domain/User.java
@@ -1,9 +1,12 @@
 package com.mauricio.workshopmongomauricio.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -19,6 +22,16 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
 
     public User(String id, String name, String email) {
